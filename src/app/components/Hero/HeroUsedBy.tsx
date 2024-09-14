8c2b07708ca0b3e192f59963e4522df5d4136f02
Arum <arum.adiningtyas@gmail.com>
2024-09-14 13:43:48 +0700

    [Section - Clients] add section

--- src/app/components/Hero/HeroUsedBy.tsx
+++ src/app/components/Hero/HeroUsedBy.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function HeroUsedBy() {
   return (
-    <div>HeroUsedBy</div>
+    <div className="hero-usedBy">
+        <div className="hero-usedBy-copy">
+            <p>The tappable story format has never been more accessible—to creators and readers alike. See what happens when Google brings stories to the open web.</p>
+        </div>
+        <div className="hero-usedBy-logos">
+            <p>Making impact at</p>
+        </div>
+    </div>
   )
 }
