3d63d6d83d503a0a09f0289ae8a2f3a74c6f3f02
Arum <arum.adiningtyas@gmail.com>
2024-09-17 16:25:27 +0700

    [Font] Add font Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Inter, Raleway } from 'next/font/google'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,6 +13,16 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
+
+const raleway = Raleway({ 
+  subsets: ['latin'],
+  variable: '--font-raleway'
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${raleway.variable} antialiased`}
       >
         {children}
       </body>
