237b952e402ea9239ee1747b7da01ebdf2180a56
Arum <arum.adiningtyas@gmail.com>
2024-09-17 16:45:30 +0700

    [Hero] add animation to hero footer

--- src/app/components/Hero/HeroFooter.tsx
+++ src/app/components/Hero/HeroFooter.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 export default function HeroFooter() {
+    const ref = useRef(null)
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ref.current,
+                start: "top center",
+                scrub: true
+            }
+        })
+
+        tl.to(".hero-container", 
+            { 
+                backgroundColor: 'white', 
+                duration: 0.25,
+            }, 
+            "-=2")
+    })
+
     return (
-        <div className="hero-text-section">
+        <div ref={ref} className="hero-text-section">
             <h1 id='hero-text'>Visual stories that feel like yours, because they are.</h1>
         </div>
     )
